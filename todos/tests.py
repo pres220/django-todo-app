98a7277a25fa60ddb302242146652e91ae509313
pres220 <58998957+pres220@users.noreply.github.com>
2020-03-12 22:32:09 -0700

    Add homepage and signup page tests

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(SimpleTestCase):
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_reverse_url(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class SignUpPageTest(SimpleTestCase):
+
+    def test_signup_status_code(self):
+        response = self.client.get('/users/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_reverse_url(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_template(self):
+        response = self.client.get(reverse('signup'))
+        self.assertTemplateUsed(response, 'signup.html')
+
+
