6adbf4e00fce845e9ef1e7a65cf30d23000677c8
pres220 <58998957+pres220@users.noreply.github.com>
2020-03-12 22:20:54 -0700

    Add user authentication and registration

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'signup.html'
+
+
